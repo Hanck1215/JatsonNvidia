469f863ce23646cf326fdd3bb626328816389f9d
Hanck1215 <st11025212@gmail.com>
2023-07-27 22:54:02 +0800

    新增css

--- JatsonNvidia/jatson/src/main/java/com/jatson/mainApplication/JatsonApplication.java
+++ JatsonNvidia/jatson/src/main/java/com/jatson/mainApplication/JatsonApplication.java
@@ -1,5 +1,6 @@
 package com.jatson.mainApplication;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class JatsonApplication {//
 
 	public static void main(String[] args) {
 		SpringApplication.run(JatsonApplication.class, args);
+		
 	}
 }
